9ec78b13841e443e2e390341695fe91d84719a2a
NellyLuo <luoning960310@gmail.com>
2020-02-02 23:05:16 +0800

    Update 11. Container With Most Water.cpp

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -15,3 +15,28 @@ public:
         return result;
     }
 };
+
+
+class Solution2 {
+public:
+    int maxArea(vector<int>& height) {
+    //从两边往中间扫，更近更短的不会带来最大值
+        int i = 0;
+        int j = height.size() - 1;
+        
+        int result = 0;
+        
+        while(i < j){
+            result = max(result,(j-i)*min(height[i],height[j]));
+            if(height[i] < height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        
+        return result;
+        
+    }
+};
