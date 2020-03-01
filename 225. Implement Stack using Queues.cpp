c69c20455216a881bf9512e9d1e1dd2c44905caa
Naristlia <luoning960310@gmail.com>
2020-03-01 09:46:36 +0800

    Create 225. Implement Stack using Queues.cpp

---
+++ 225. Implement Stack using Queues.cpp
@@ -0,0 +1,45 @@
+class MyStack {
+private:
+    queue<int> q;
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        int m = q.size();
+        q.push(x);
+        for(int i=0;i<m;i++){
+            int n = q.front();
+            q.pop();
+            q.push(n);
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int p = q.front();
+        q.pop();
+        return p;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
